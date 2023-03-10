16c9496369049fa9abb293a1117e604009c7c7b8
Alejandro Gil <alejandro115935@gmail.com>
2023-03-10 15:32:31 -0600

    Inicio

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Laboratorio5EST1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ingrese el archivo .Json");
+        }
+    }
+}
